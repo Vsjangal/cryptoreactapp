9e18f422841611edb6d04ab799f7fe6ea7e1eb6d
Vsjangal <87491877+Vsjangal@users.noreply.github.com>
2023-06-11 13:27:17 +0530

    Update and rename apiEndpoints.js to api.js

---
+++ api.js
@@ -0,0 +1,8 @@
+const endpoints = {
+  bitcoinWalletImport: 'https://example.com/bitcoin/wallet/import',
+  polygonWalletImport: 'https://example.com/polygon/wallet/import',
+  livePrices: 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,usdt&vs_currencies=usd',
+  transactionHistory: 'https://example.com/transaction/history',
+};
+
+export default endpoints;
