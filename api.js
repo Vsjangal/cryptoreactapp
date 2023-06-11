bf49739cd5de7c4536f0105b43bf00c72fc15e80
Vsjangal <87491877+Vsjangal@users.noreply.github.com>
2023-06-11 13:31:23 +0530

    Update api.js

--- api.js
+++ api.js
@@ -1,8 +1,8 @@
 const endpoints = {
-  bitcoinWalletImport: 'https://example.com/bitcoin/wallet/import',
-  polygonWalletImport: 'https://example.com/polygon/wallet/import',
+  bitcoinWalletImport: 'https://api.blockchain.com/v3/import/wallet',
+  polygonWalletImport: 'https://example.com/polygon/wallet/import', // Replace with the actual API endpoint for Polygon wallet import
   livePrices: 'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,usdt&vs_currencies=usd',
-  transactionHistory: 'https://example.com/transaction/history',
+  transactionHistory: 'https://example.com/transaction/history', // Replace with the actual API endpoint for transaction history
 };
 
 export default endpoints;
